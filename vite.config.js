bc2a63a8a18cafe6f78db152dec188fceedc0288
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2025-06-03 20:04:48 +0100

    update homepage URL in package.json and format vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/app/",
-  plugins: [
-    react(),
-    tailwindcss(),
-    
-  ],
-    server: {
-    historyApiFallback: true, // <--- Important
-  },
-})
+	base: "/app/",
+	plugins: [react(), tailwindcss()],
+	server: {
+		historyApiFallback: true, // <--- Important
+	},
+	preview: {
+		allowedHosts: ["wassiya.rechidiahmed.me"],
+	},
+});
